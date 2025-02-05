155ee8c080cf7b478e5c891ac567473a4d0d8d4e
FuriousKermit <vndnsla12@gmail.com>
2025-02-05 23:28:20 +1100

    remove createMessage

--- index.js
+++ index.js
@@ -18,15 +18,3 @@ new CommandHandler({
 client.login(process.env.TOKEN);
 
 client.once('ready', () => console.log(client.user.tag+'6준비 완료!9'));
-
-client.on('messageCreate', (msg) => {
-    console.log(`messageCreate: ${msg}`);
-    const cmd = msg.content
-    if(msg.author.bot) return
-    if(cmd == 'ㅎㅇ') msg.reply('안녕하세요!')
-    if(cmd == '노모어') msg.channel.send('바나나')
-    if(cmd == '삭제') {
-        msg.delete()
-        msg.reply('6삭제 완료!9')
-    }
-});
